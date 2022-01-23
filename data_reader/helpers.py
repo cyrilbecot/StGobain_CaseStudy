802b5f837178a5ede0303b3f5ca9f52d439a3b30
Cyril Pascal Becot <cyril.becot@cern.ch>
2022-01-23 16:27:21 +0100

    Add helper function to locate files

--- data_reader/helpers.py
+++ data_reader/helpers.py
@@ -10,3 +10,13 @@ class DataTypes(Enum):
 def get_read_fcn(dt: DataTypes):
     if dt == DataTypes.Excel:
         return pandas.read_csv
+
+def locations():
+    res={
+            'referundum':'/input_data/Referundum.csv',
+            'geodata':'/input_data/GeoShapeCommunes/communes-20220101.shp',
+            'recensement':'/input_data/base-cc-evol-struct-pop-2013/base-cc-evol-struct-pop-2013.xls',
+            'revenus':'/input_data/filo-revenu-pauvrete-menage-2013/filo-revenu-pauvrete-menage-2013.xls',
+            'diplomes':'/input_data/pop-16ans-dipl6817/pop-16ans-dipl6817.xlsx'
+        }
+    return res
