21259a965ba3d0a1e96804a1abf8cb62d985b816
Cyril Becot <17175722+cyrilbecot@users.noreply.github.com>
2022-01-29 14:46:23 +0100

    Update cmd_jupyter_in_docker.sh

--- cmd_jupyter_in_docker.sh
+++ cmd_jupyter_in_docker.sh
@@ -1,4 +1,5 @@
 # docker build . --file Dockerfile --tag stgobain-casestudy:latest # This one is to be used to build the container
 
 # This one runs the container, launching a Jupiter Notebook hub at /
+sudo service docker start
 docker run -v $PWD:/Work -p 8888:8888 -it stgobain-casestudy
