74021ec101d81cfd9ccf81dc9c3e5cc644c7ac1e
Cyril Pascal Becot <cyril.becot@cern.ch>
2022-01-29 20:30:27 +0100

    Helper to read age data

--- data_reader/helpers.py
+++ data_reader/helpers.py
@@ -17,3 +17,27 @@ def locations(which):
             'diplomes':'/input_data/pop-16ans-dipl6817/pop-16ans-dipl6817.xlsx'
         }
     return res[which]
+
+def age_class():
+    res = {
+        '0014': (0,14),
+        '1529': (15,29),
+        '3044': (29,44),
+        '4559': (45,59),
+        '6074': (60,74),
+        '7589': (75,89),
+        '90P': (90,90)
+          }
+    return res
+
+def mean_age(x,suffix='P13_POP'):
+    tot_pop=0.
+    tot_age=0.
+
+    for idx,val in age_class().items():
+        current_pop = x[suffix+idx]
+        age=(val[0]+val[1])/2.
+        tot_age += current_pop*age
+        tot_pop += current_pop
+
+    return tot_age/tot_pop
